ebabb8edaad97d70f30254410077b5f99fb7953b
AJBOI18 <ajdelfinmontebonjr15@gmail.com>
2025-05-05 21:59:52 +0800

    Create guests.js

---
+++ guests.js
@@ -0,0 +1,7 @@
+// guests.js
+const guests = [
+  { name: "Juan Dela Cruz", table: "1", chair: "A" },
+  { name: "Maria Santos", table: "1", chair: "B" },
+  { name: "Pedro Reyes", table: "2", chair: "A" },
+  { name: "Anna Lopez", table: "2", chair: "B" }
+];
