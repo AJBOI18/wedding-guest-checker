d2a57ff4c97263e564635f360dc2526309355560
AJBOI18 <ajdelfinmontebonjr15@gmail.com>
2025-05-06 16:20:33 +0800

    Update guests.js

--- guests.js
+++ guests.js
@@ -3,7 +3,7 @@ const guests = [
   { name: "Ma.Belen Montebon", table: "1", chair: "A" },
   { name: "Asterio Montebon Sr", table: "1", chair: "B" },
   { name: "Asterio Montebon Jr", table: "1", chair: "C" },
-  { name: "Princess Ronamie Otarra", table: "1", chair: "D" }
-  { name: "Ma. Arlene M. Jimenez", table: "1", chair: "E" }
+  { name: "Princess Ronamie Otarra", table: "1", chair: "D" },
+  { name: "Ma. Arlene M. Jimenez", table: "1", chair: "E" },
   { name: "Aimie M. Molejon", table: "2", chair: "A" }
 ];
